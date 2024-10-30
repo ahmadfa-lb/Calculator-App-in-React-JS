933bc065580d2fb810ef5322d7559d4a914093a8
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2024-10-30 20:56:06 +0200

    adding some functions to handle onClick buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,70 @@
+import { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
 
-const getStyleName = btn =>{
-    const className = {
-        '=': 'equals',
-        'X': 'opt',
-        '_': 'opt',
-        '+': 'opt',
-        '/': 'opt',
-    }
-    return className[btn];
-}
+const getStyleName = (btn) => {
+  const className = {
+    "=": "equals",
+    X: "opt",
+    _: "opt",
+    "+": "opt",
+    "/": "opt",
+  };
+  return className[btn];
+};
 
 const Button = ({ value }) => {
-  return <button className={`${getStyleName(value)} button`}>{value}</button>;
+    
+    const { calc, setCalc } = useContext(CalcContext)
+    
+    // User click Comma
+    const commaClick = () =>{
+        setCalc({
+            ...calc,
+            num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+        })
+    }
+    // User Click C
+    const resetClick = () =>{
+        setCalc({sign : '', num: 0, res:0})
+    }
+    // User click number
+    const handleClickbutton = () =>{
+        const numberString = value.toString();
+
+        let numberValue;
+        if(numberString === '0' && calc.num === 0){
+            numberValue = "0";
+        } else{
+            numberValue = Number(calc.num + numberString);
+        }
+
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
+    }
+
+    const handleBtnClick = () => {
+        const results = {
+            '.': commaClick,
+            'C': resetClick,
+        }
+        if(results[value]){
+            return results[value]()
+        } else {
+            return handleClickbutton()
+        }
+        
+    }
+
+  return (
+    <button
+      onClick={handleBtnClick}
+      className={`${getStyleName(value)} button`}
+    >
+      {value}
+    </button>
+  );
 };
 
 export default Button;
