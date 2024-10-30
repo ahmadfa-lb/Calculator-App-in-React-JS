5604825388897f6c523d2aed0276759ecf5384f8
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2024-10-30 19:22:07 +0200

    implementing the CalcContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Wrapper from './components/Wrapper'
 import Screen from './components/Screen'
 import ButtonBox from './components/ButtonBox';
 import Button from './components/Button';
+import CalcProvider from './context/CalcContext'
 
 const BtnValues = [
   ["C", "+-", "%", "/"],
@@ -17,7 +18,7 @@ function App() {
   
 
   return (
-    <div>
+    <CalcProvider>
       <Wrapper>
         <Screen />
         <ButtonBox>
@@ -29,7 +30,7 @@ function App() {
           ))}   
         </ButtonBox>        
       </Wrapper>
-    </div>
+      </CalcProvider>
   )
 }
 
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import CalcContext from "../context/CalcContext";
+
 const Screen = () => {
+    
+    const { calc } = useContext(CalcContext)
+    
     return (
         <div className="screen">1424124214124</div>
     )
